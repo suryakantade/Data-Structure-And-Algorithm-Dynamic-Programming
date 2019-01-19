934e81c077ccd809bf1b844d6040085295258f64
sde <skantade@mscripts.com>
2019-01-20 03:32:18 +0530

    na

---
+++ DataStructureAndAlgo/src/com/surya/graph/GraphRepresentationOwnStyle.java
@@ -0,0 +1,29 @@
+package com.surya.graph;
+
+import java.util.HashSet;
+
+class Node<E>{
+	
+}
+
+class Garph<E>{
+	Node<E> node ;
+	HashSet<E> connectedVertex ;
+	public Garph(Node<E> node, HashSet<E> connectedVertex) {
+		super();
+		this.node = node;
+		this.connectedVertex = connectedVertex;
+	}
+	
+	
+	
+}
+
+public class GraphRepresentationOwnStyle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
